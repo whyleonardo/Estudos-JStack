4d121bf81e8ef324a589a40af4d81b5dc596a0db
whyleonardo <pleasefloyd73@gmail.com>
2023-03-02 22:46:15 -0300

    hofs some and every

--- JS Moderno/09-hof.js
+++ JS Moderno/09-hof.js
@@ -4,7 +4,14 @@ const array = [
   { product: 'Magic Mouse', price: 1000, quantity: 5 }
 ]
 
+// find & findIndex
 const find = array.find(({ price }) => price > 3500)
 const findIndex = array.findIndex(({ price }) => price >= 20000)
 
-console.log(findIndex)
+// some && every
+const some = array.some(({ price }) => price > 3500)
+const every = array.every(({ price }) => price >= 1000)
+
+
+
+console.log(every)
