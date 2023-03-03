58577f485b00b74c8e4df2ffda3858dbf3ff3b1d
whyleonardo <pleasefloyd73@gmail.com>
2023-03-02 23:38:46 -0300

    hofs reduce

--- JS Moderno/09-hof.js
+++ JS Moderno/09-hof.js
@@ -1,7 +1,7 @@
 const array = [
-  { product: 'iPhone', price: 5000, quantity: 2 },
-  { product: 'MacBook Pro', price: 20000, quantity: 1 },
-  { product: 'Magic Mouse', price: 1000, quantity: 5 }
+  { name: 'iPhone', price: 5000, quantity: 2 },
+  { name: 'MacBook Pro', price: 20000, quantity: 1 },
+  { name: 'Magic Mouse', price: 1000, quantity: 5 }
 ]
 
 // find & findIndex
@@ -18,4 +18,8 @@ const map = array.map((product) => ({ ...product, subtotal: product.price * prod
 // filter
 const filter = array.filter((product) => product.quantity > 1)
 
-console.log(filter)
+// reduce
+const reduce = array.reduce((acc, product) => (acc + (product.price * product.quantity)), 0)
+
+console.log({ reduce })
+
